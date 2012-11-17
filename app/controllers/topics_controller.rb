880dbda19579192079d0ec9036c412e0d26bebb2
David Ladowitz <david@ladowitz.com>
2012-11-17 01:45:58 -0800

    finishe app

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,6 +3,7 @@ class TopicsController < ApplicationController
   # GET /topics.json
   def index
     @topics = Topic.all
+    @topics = @topics.sort_by { |topic| topic.votes.count }.reverse
 
     respond_to do |format|
       format.html # index.html.erb
